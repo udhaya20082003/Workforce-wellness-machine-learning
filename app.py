69e9c08f122996fc204b4bec675e88c209f92316
udhaya20082003 <123627902+udhaya20082003@users.noreply.github.com>
2024-12-03 09:49:58 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+from flask import jsonify
+import sqlite3 as lite 
+
+# Set up Flask
+app = Flask(__name__)
+
+# Set up app routes
+@app.route("/")
+def index():
+    return render_template("index.html")
+    #return "Hello flask"
+
+@app.route('/api/v1.0/data', methods=['GET'])
+def get_data():
+    con = lite.connect('mental_health.db')
+    with con:
+        cur = con.cursor()
+        cur.execute("SELECT * FROM pre_encoded_questions")
+        rows = cur.fetchall()
+        result = []
+        for row in rows:
+            result.append(row)
+    return jsonify({'data': result})
+
+# Tell Flask to run
+if __name__ == "__main__":
+    app.run()
